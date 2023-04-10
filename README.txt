6efb9bbd76c340c23146667e99cde68e78259597
Marianna Zurek <zurekmarianna@gmail.com>
2023-04-10 20:24:34 +0200

    update

--- README.txt
+++ README.txt
@@ -11,8 +11,8 @@ The aim of the project is to simulate different colored balls moving in a random
 The Ball.js component is responsible for the balls that appear in the simulation.
 The Edges.js component represents its boundaries.
 Bouncing occurs relative to the balls and relative to the walls.
-In order to simulate infinite the bouncing of the balls, the @react-three/fiber and @react-three/cannon libraries were used.
-The problem encountered was the placement of the balls in a circle, so the simulation was presented in a square instead using useFrame and useBox hooks.
+In order to simulate the infinite bouncing of the balls, the @react-three/fiber and @react-three/cannon libraries were used.
+The problem encountered was the placement of the balls in a circle, so the simulation was presented in a square instead.
 
 In order to start development of the project please type "npm install" in the terminal to install all the dependencies and "npm start" to start the project.
 
--- src/components/Edges.js
+++ src/components/Edges.js
@@ -1,111 +1,89 @@
 import React from "react";
-import { useFrame } from "@react-three/fiber";
 import { useBox } from "@react-three/cannon";
 import { angleToRadians } from "../utils/angleToRadians";
 
-function Right(props) {
-  const { args = [0.1, 20, 0.4] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(10, 0, 0);
-    api.rotation.set(0, 0, 0);
-  });
+const Right = () => {
+  const [ref] = useBox(() => ({
+    args: [0.1, 20, 0.4],
+    position: [10, 0, 0],
+  }));
 
   return (
     <mesh ref={ref}>
-      <boxGeometry args={args} />
+      <boxGeometry args={[0.1, 20, 0.2]} />
       <meshStandardMaterial color={"#fff"} />
     </mesh>
   );
-}
+};
 
-function Left(props) {
-  const { args = [0.1, 20, 0.4] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(-10, 0, 0);
-    api.rotation.set(0, 0, 0);
-  });
+const Left = () => {
+  const [ref] = useBox(() => ({
+    args: [0.1, 20, 0.4],
+    position: [-10, 0, 0],
+  }));
 
   return (
     <mesh ref={ref}>
-      <boxGeometry args={args} />
+      <boxGeometry args={[0.1, 20, 0.2]} />
       <meshStandardMaterial color={"#fff"} />
     </mesh>
   );
-}
+};
 
-
-
-function Top(props) {
-  const { args = [0.1, 20, 0.4] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(0, 10, 0);
-    api.rotation.set(0, 0, angleToRadians(90));
-  });
+const Top = () => {
+  const [ref] = useBox(() => ({
+    args: [0.1, 20, 0.4],
+    position: [0, 10, 0],
+    rotation: [0, 0, angleToRadians(90)],
+  }));
 
   return (
     <mesh ref={ref}>
-      <boxGeometry args={args} />
+      <boxGeometry args={[0.1, 20, 0.2]} />
       <meshStandardMaterial color={"#fff"} />
     </mesh>
   );
-}
+};
 
-function Bottom(props) {
-  const { args = [0.1, 20, 0.4] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(0, -10, 0);
-    api.rotation.set(0, 0, angleToRadians(90));
-  });
+const Bottom = () => {
+  const [ref] = useBox(() => ({
+    args: [0.1, 20, 0.4],
+    position: [0, -10, 0],
+    rotation: [0, 0, angleToRadians(90)],
+  }));
 
   return (
     <mesh ref={ref}>
-      <boxGeometry args={args} />
+      <boxGeometry args={[0.1, 20, 0.2]} />
       <meshStandardMaterial color={"#fff"} />
     </mesh>
   );
-}
+};
 
-function Back(props) {
-  const { args = [20, 20, 1] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(0, 0, -1);
-    api.rotation.set(0, 0, 0);
-  });
+const Back = () => {
+  const [ref] = useBox(() => ({
+    args: [20, 20, 1],
+    position: [0, 0, -1],
+  }));
 
   return (
     <mesh ref={ref}>
-      <boxGeometry args={args} />
+      <boxGeometry args={[20, 20, 1]} />
       <meshStandardMaterial color={"#fff"} transparent={true} opacity={0} />
     </mesh>
   );
-}
+};
 
-function Front(props) {
-  const { args = [20, 20, 1] } = props;
-  const [ref, api] = useBox(() => ({ args }));
-
-  useFrame(() => {
-    api.position.set(0, 0, 1);
-    api.rotation.set(0, 0, 0);
-  });
+const Front = () => {
+  const [ref] = useBox(() => ({ args: [20, 20, 1], position: [0, 0, 1] }));
 
   return (
     <mesh ref={ref}>
-      <planeGeometry args={args} />
+      <boxGeometry args={[20, 20, 1]} />
       <meshStandardMaterial color={"#fff"} transparent={true} opacity={0} />
     </mesh>
   );
-}
+};
 
 const Edges = () => {
   return (
